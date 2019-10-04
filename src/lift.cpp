2ec881d22832e78cb2c28d71d193b6d19f391581
Charles <sazrocks6@gmail.com>
2019-10-03 18:43:56 -0700

    Added basic code for lift solenoids

--- src/lift.cpp
+++ src/lift.cpp
@@ -3,6 +3,12 @@
 //
 #include "main.h"
 int liftTarget = 10;
+
+pros::ADIDigitalOut pistonFloor (7);
+pros::ADIDigitalOut pistonDoor (8);
+
+bool floorActivated = false;
+bool doorActivated = false;
 void liftTeleop(){
     if(master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_UP)){
         liftTarget += 25;
@@ -21,6 +27,16 @@ void liftTeleop(){
     }
     else{
         lift_motors.moveAbsolute(liftTarget, 50);
+    }
+
+    if(master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_L1)){
+        doorActivated = !doorActivated;
+        pistonDoor.set_value(doorActivated);
+    }
+
+    if(master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_L2)){
+        floorActivated = !floorActivated;
+        pistonFloor.set_value(floorActivated);
     }
 
 
